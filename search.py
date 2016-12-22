cb988ba7f958c3a86fe05e503272775b6541d89b
VincentZhengg <vincentzheng73@outlook.com>
2016-12-22 15:01:12 +0800

    add get word sentence method

--- search.py
+++ search.py
@@ -4,7 +4,14 @@ filename = "The.Big.Bang.Theory.S10E11.720p.HDTV.X264-DIMENSION.HI.srt"
 search_word = ["possum", "wheezy", "pedestrian", "sprinkle"]
 
 
-class Parse:
+class ParseSegmentException(Exception):
+    """
+    This exception is raised if the file segment is not parsed correctly. We assume that subtitle
+    file is split by '\n', if this is not the case, this exception may be raise.
+    """
+
+
+class Subtitle:
 
     def __init__(self, filename):
         self.filename = filename
@@ -19,7 +26,7 @@ class Parse:
                     yield segment
                     segment = []
 
-    def get_full_sentence(self):
+    def get_all_sentences(self):
         punctuation = (".", "?")
         sentence = ''
         for segment in self.get_segment():
@@ -31,19 +38,20 @@ class Parse:
                 sentence = ''
 
     def parse_segment(self, segment):
-        num = segment[0]
-        time = segment[1]
-        string = " ".join(segment[2:])
-        return num, time, string
+        try:
+            num = segment[0]
+            time = segment[1]
+            string = " ".join(segment[2:])
+            return num, time, string
+        except IndexError:
+            raise ParseSegmentException
+
+    def get_word_sentence(self, word):
+        match_list = []
+        for sentence in self.get_all_sentences():
+            match = re.search(word, sentence)
+            if match is not None:
+                match_list.append(sentence)
+        return "\n **************\n".join(match_list)
 
 
-def get_re_str(search_word):
-    return "|".join(search_word)
-
-
-parse = Parse(filename=filename)
-for sentence in parse.get_full_sentence():
-    re_str = get_re_str(search_word)
-    match = re.search(re_str, sentence)
-    if match is not None:
-        print(match.group(), sentence)
